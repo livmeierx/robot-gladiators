ef5dcc75e33e2f48674335ac7c19d53db7c70c58
livmeierx <liv.meier13@gmail.com>
2022-05-17 17:21:36 -0400

    updates

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,7 +10,39 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function() {
+  // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
-};
 
-fight();
+  enemyHealth = enemyHealth - playerAttack;
+  // check enemy's health
+  if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+  } 
+  else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+
+  console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+  );
+
+  playerHealth = playerHealth - enemyAttack;
+
+  console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+
+// check player's health
+if (playerHealth <= 0) {
+  window.alert(playerName + " has died!");
+} 
+else {
+  window.alert(playerName + " still has " + playerHealth + " health left.");
+}
+
+  console.log(
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+  );
+
+  console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+  );
+};
